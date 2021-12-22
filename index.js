ed813e58b74d0a9a16237b05edde540cf87773c4
jaehyunlee <jaehyunlee25@gmail.com>
2021-12-23 02:34:20 +0900

    renew

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const log = console.log;
 
 const every5seconds = '*/5 * * * * *';
 cron.schedule(every5seconds, function () {
-  log('매 5초 마다 작업 실행 :', new Date().toString());
+  const now = new Date();
+  now.setHours(now.getHours() +9);  //set 'Asia/Seoul'
+  log('매 5초 마다 작업 실행 :', now.toString());
 });
 
 cron.schedule('0 0 3 * * *', function () {
